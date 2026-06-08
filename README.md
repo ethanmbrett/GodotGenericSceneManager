b7f631ba0c7fa2025a6a45e4a1be89c7d6e98136
ethanmbrett <ethanmbrett@gmail.com>
2026-06-08 14:16:03 -0400

    Added resource loading

--- README.md
+++ README.md
@@ -1,29 +1,34 @@
 ## Copyright notice
 Copyright (c) 2026 Ethan Brett  
-Licensed under the MIT License. See LICENSE file in the project root for full license information
+Licensed under the MIT License. See LICENSE file in the project root for full license information  
 
 # GodotGenericSceneManager
-Simple scene manager system for Godot projects.
+Simple scene manager system for Godot projects.  
 
 ## Use Cases
-This scene manager is largely meant to avoid the project corruption that tends to occur when scenes directly reference each other in Godot. This project currently uses asynchronous loading exclusively, but it does not yet include fixes for the hangs that can occur when asynchronously loading larges scene files.
+This scene manager is largely meant to avoid the project corruption that tends to occur when scenes directly reference each other in Godot. This project currently uses asynchronous loading exclusively, but it does not yet include fixes for the hangs that can occur when asynchronously loading larges scene files.  
 
 ## Installation
-Drag the addons folder from this plugin into the resources folder for your project. Go into the plugins tab of your project settings and enable this plugin. Open the SceneManager scene file in the "Res://addons/Singletons" folder and add all scenes you would like to be immediately accessible to the scene loader to its "Initialize Scenes" field. Any of these scenes can now be loaded through the SceneManager.
+Drag the addons folder from this plugin into the resources folder for your project. Go into the plugins tab of your project settings and enable this plugin. Open the SceneManager scene file in the "Res://addons/Singletons" folder and add all scenes you would like to be immediately accessible to the scene loader to its "Initialize Scenes" field. Any of these scenes can now be loaded through the SceneManager.  
 
 ## Loading Scenes
-To load a scene from code, you must follow the following steps:
--Use "SceneManager.StartSceneLoad("SCENE NAME TO LOAD")" to initialize the load
--Use "SceneManager.CheckSceneLoadStatus()" to check the load status in a process function. When this function returns 1.0, the scene is loaded and ready to instantiate. If it returns -1.0, the load failed.
--Once the above function returns 1.0, use "SceneManager.FinalizeSceneLoad()" to finally instance the scene as the new tree root.
+To load a scene from code, you must follow the following steps:  
+-Use "SceneManager.StartSceneLoad("SCENE NAME TO LOAD")" to initialize the load  
+-Use "SceneManager.CheckSceneLoadStatus()" to check the load status in a process function. When this function returns 1.0, the scene is loaded and ready to instantiate. If it returns -1.0, the load failed  
+-Once the above function returns 1.0, use "SceneManager.FinalizeSceneLoad()" to finally instance the scene as the new tree root  
+
+## Loading Resources
+The process of loading resources is much the same as for loading a new scene, with the notable exception that more than one resource can be loaded at a time, and that the resource must be checked and retrieved by path.  
+-Use "SceneManager.StartResourceLoad("RESOURCE PATH TO LOAD")" to initialize the load  
+-Use "SceneManager.CheckResourceLoadStatus("RESOURCE PATH TO LOAD") to check the load status in a process function. When this function returns 1.0, the resource is loaded and ready to instantiate. If it returns -1.0, the load failed  
+-Once the above function returns 1.0, use the return value of "SceneManager.FinalizeResourceLoad("RESOURCE PATH TO LOAD")" to retrieve the resource  
 
 ## Other Usage
-The SceneManager autoload contains functions for Adding and Removing scenes from the registry at runtime, as well as changing the packed scene that a key references. These function are available both with both single-scene and mass variants. Their signatures can be found in "res://addons/SimpleSceneManager/Singletons/SceneManager.gd"
+The SceneManager autoload contains functions for Adding and Removing scenes from the registry at runtime, as well as changing the packed scene that a key references. These function are available both with both single-scene and mass variants. Their signatures can be found in "res://addons/SimpleSceneManager/Singletons/SceneManager.gd"  
 
 ## Roadmap
 This plugin is not yet feature-complete and will be expanded to allow to include new features, following this roadmap:  
-
-v0.3: Add asynchronous loading -- Current Version  
-v0.4: Add resource loading  
+ 
+v0.4: Add resource loading -- Current Version  
 v0.5: Add chunked scene loading. Feature-complete, beta release  
 v1.0: Testing, cleanup, and documentation. Full release  
--- addons/SimpleSceneManager/Singletons/SceneManager.gd
+++ addons/SimpleSceneManager/Singletons/SceneManager.gd
@@ -9,8 +9,6 @@ extends Node
 var SceneRegistry: Dictionary[String, String]
 # The name of any scene currently being loaded. Set to be empty if none.
 var CurrentLoadingScenePath: String = ""
-# Whether a scene has completed loading but hasn't been made scene root
-var SceneReadyToInstance: bool = false
 
 @export_group("Initialization")
 # List of scene names and file paths to initialize to the SceneManager
@@ -29,36 +27,70 @@ func StartSceneLoad(SceneName: String):
 	ResourceLoader.load_threaded_request(CurrentLoadingScenePath)
 
 # One of the bread-and-butter function of the manager. Checks the current status of a scene load operation
-# Returns -1.0 if load is not in progress or fails
+# Returns -1.0 if load is not in progress or fails, else returns the progress of loading the scene
 func CheckSceneLoadStatus() -> float:
 	if(CurrentLoadingScenePath == ""):
-		push_error("Attempting to check status of load operation when no load is in progress!")
+		push_error("Attempting to check status of scene load operation when no load is in progress!")
 		return -1.0
 	var LoaderProgress: Array
 	var LoaderStatus = ResourceLoader.load_threaded_get_status(CurrentLoadingScenePath, LoaderProgress)
 	if (LoaderStatus == ResourceLoader.ThreadLoadStatus.THREAD_LOAD_FAILED or len(LoaderProgress) < 1):
-		push_error("Load operation failed!")
+		push_error("Scene load operation failed!")
 		return -1.0
-	if (LoaderStatus == ResourceLoader.ThreadLoadStatus.THREAD_LOAD_FAILED):
-		push_error("Load operation failed due to invalid resource!")
+	if (LoaderStatus == ResourceLoader.ThreadLoadStatus.THREAD_LOAD_INVALID_RESOURCE):
+		push_error("Scene load operation failed due to invalid resource!")
 		return -1.0
-	if (LoaderStatus == ResourceLoader.ThreadLoadStatus.THREAD_LOAD_LOADED):
-		SceneReadyToInstance = true
 	return LoaderProgress[0]
 
 # One of the bread-and-butter function of the manager. Finalizes the load operation and sets the loaded scene
 # to be the new tree root.
 func FinalizeSceneLoad():
 	if(CurrentLoadingScenePath == ""):
-		push_error("Attempting to finish load operation when no load is in progress!")
+		push_error("Attempting to finish scene load operation when no scene load is in progress!")
 		return
-	if(!SceneReadyToInstance):
-		push_error("Attempting to finalize load operation before load operation is complete!")
+	if(ResourceLoader.load_threaded_get_status(CurrentLoadingScenePath) != ResourceLoader.ThreadLoadStatus.THREAD_LOAD_LOADED):
+		push_error("Attempting to finalize scene load operation before the scene load operation is complete!")
 		return
 	var LoadedScene: PackedScene = ResourceLoader.load_threaded_get(CurrentLoadingScenePath)
 	CurrentLoadingScenePath = ""
-	SceneReadyToInstance = false
 	get_tree().change_scene_to_packed.call_deferred(LoadedScene)
+
+# Function for loading individual resources asynchronously. Starts the load operation
+func StartResourceLoad(ResourcePath: String):
+	if(!ResourceLoader.exists(ResourcePath)):
+		push_error("Attempting load resource with invalid path: " + ResourcePath)
+		return
+	ResourceLoader.load_threaded_request(ResourcePath)
+
+# Function for loading individual resources asynchronously. Checks the status of a load operation
+# Returns -1.0 and throws an error if the operation fails, else returns the progress of the operation
+func CheckResourceLoadStatus(ResourcePath: String) -> float:
+	if(!ResourceLoader.exists(ResourcePath)):
+		push_error("Attempting to check resource load operation with invalid path: " + ResourcePath)
+		return -1.0
+	var LoaderProgress: Array
+	var LoaderStatus: ResourceLoader.ThreadLoadStatus = ResourceLoader.load_threaded_get_status(ResourcePath, LoaderProgress)
+	if(LoaderStatus == ResourceLoader.ThreadLoadStatus.THREAD_LOAD_INVALID_RESOURCE):
+		push_error("Attempting to check resource load operation without starting the load operation: " + ResourcePath)
+		return -1.0
+	if(LoaderStatus == ResourceLoader.ThreadLoadStatus.THREAD_LOAD_FAILED or len(LoaderProgress) < 1):
+		push_error("Resource load failed: " + ResourcePath)
+		return -1.0
+	return LoaderProgress[0]
+
+# Function for loading individual resources asynchronously. Returns a loaded resource
+# Returns null and throws an error if the operation fails, else returns resource
+func FinalizeResourceLoad(ResourcePath: String) -> Resource:
+	if(!ResourceLoader.exists(ResourcePath)):
+		push_error("Attempting to finalize resource load operation with invalid path: " + ResourcePath)
+		return null
+	if(ResourceLoader.load_threaded_get_status(ResourcePath) == ResourceLoader.ThreadLoadStatus.THREAD_LOAD_INVALID_RESOURCE):
+		push_error("Attempting to finalize resource load operation without starting the load operation: " + ResourcePath)
+		return null
+	if(ResourceLoader.load_threaded_get_status(ResourcePath) != ResourceLoader.ThreadLoadStatus.THREAD_LOAD_LOADED):
+		push_error("Attempting to finalize resource load operation before the resource load operation is complete: " + ResourcePath)
+		return null
+	return ResourceLoader.load_threaded_get(ResourcePath)
 
 # Adds a single scene to the registry. Mostly used as a helper
 # function by the function that does this en mass.
