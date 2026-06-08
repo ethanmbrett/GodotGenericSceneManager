656c785eba3be321b6f1cb698f487c0ab88b5d7c
ethanmbrett <ethanmbrett@gmail.com>
2026-06-08 13:38:56 -0400

    Updated to use asynchronous loading

--- README.md
+++ README.md
@@ -6,24 +6,24 @@ Licensed under the MIT License. See LICENSE file in the project root for full li
 Simple scene manager system for Godot projects.
 
 ## Use Cases
-This scene manager is largely meant to avoid the project corruption that tends to occur when scenes directly reference
-each other in Godot. This plugin doesn't yet handle large-scale, continuous loading, such as would require a loading screen.
+This scene manager is largely meant to avoid the project corruption that tends to occur when scenes directly reference each other in Godot. This project currently uses asynchronous loading exclusively, but it does not yet include fixes for the hangs that can occur when asynchronously loading larges scene files.
 
 ## Installation
 Drag the addons folder from this plugin into the resources folder for your project. Go into the plugins tab of your project settings and enable this plugin. Open the SceneManager scene file in the "Res://addons/Singletons" folder and add all scenes you would like to be immediately accessible to the scene loader to its "Initialize Scenes" field. Any of these scenes can now be loaded through the SceneManager.
 
 ## Loading Scenes
-To load a scene from code, use the following function signature: "SceneManager.LoadSceneFromRegistry("MySceneName")"
+To load a scene from code, you must follow the following steps:
+-Use "SceneManager.StartSceneLoad("SCENE NAME TO LOAD")" to initialize the load
+-Use "SceneManager.CheckSceneLoadStatus()" to check the load status in a process function. When this function returns 1.0, the scene is loaded and ready to instantiate. If it returns -1.0, the load failed.
+-Once the above function returns 1.0, use "SceneManager.FinalizeSceneLoad()" to finally instance the scene as the new tree root.
 
 ## Other Usage
-The SceneManager autoload contains functions for Adding and Removing scenes from the registry at runtime, as well as changing
-the packed scene that a key references. These function are available both with both single-scene and mass variants. Their signatures can be found in "res://addons/SimpleSceneManager/Singletons/SceneManager.gd"
+The SceneManager autoload contains functions for Adding and Removing scenes from the registry at runtime, as well as changing the packed scene that a key references. These function are available both with both single-scene and mass variants. Their signatures can be found in "res://addons/SimpleSceneManager/Singletons/SceneManager.gd"
 
 ## Roadmap
-This plugin is not yet feature-complete, and will be expanded to allow to include new features, following this roadmap:  
+This plugin is not yet feature-complete and will be expanded to allow to include new features, following this roadmap:  
 
-v0.2: Remove need for the initializer -- Current Version  
-v0.3: Add asynchronous loading  
+v0.3: Add asynchronous loading -- Current Version  
 v0.4: Add resource loading  
 v0.5: Add chunked scene loading. Feature-complete, beta release  
 v1.0: Testing, cleanup, and documentation. Full release  
--- addons/SimpleSceneManager/Singletons/SceneManager.gd
+++ addons/SimpleSceneManager/Singletons/SceneManager.gd
@@ -6,51 +6,100 @@
 extends Node
 
 # The list of loadable scenes
-var SceneRegistry: Dictionary[String, PackedScene]
+var SceneRegistry: Dictionary[String, String]
+# The name of any scene currently being loaded. Set to be empty if none.
+var CurrentLoadingScenePath: String = ""
+# Whether a scene has completed loading but hasn't been made scene root
+var SceneReadyToInstance: bool = false
 
 @export_group("Initialization")
-# List of scene names and files to send to the SceneManager
-# Modify in the default version scene as necessary
-@export var InitializeScenes: Dictionary[String, PackedScene]
+# List of scene names and file paths to initialize to the SceneManager
+@export var InitializeScenes: Dictionary[String, String]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	AddScenesToRegistry(InitializeScenes)
 
-# Bread-and-butter function of the manager. Loads a scene by name
-# and makes it the new scene root.
-func LoadSceneFromRegistry(SceneName: String):
+# One of the bread-and-butter function of the manager. Starts loading a scene through the Resource Loader
+func StartSceneLoad(SceneName: String):
 	if(SceneRegistry.keys().find(SceneName) == -1):
-		push_error("Attempting to load unregistered scene!")
-		#get_tree().quit()
-	get_tree().change_scene_to_packed.call_deferred(SceneRegistry[SceneName])
+		push_error("Attempting to load unregistered scene with name: " + SceneName)
+		return
+	CurrentLoadingScenePath = SceneRegistry[SceneName]
+	ResourceLoader.load_threaded_request(CurrentLoadingScenePath)
+
+# One of the bread-and-butter function of the manager. Checks the current status of a scene load operation
+# Returns -1.0 if load is not in progress or fails
+func CheckSceneLoadStatus() -> float:
+	if(CurrentLoadingScenePath == ""):
+		push_error("Attempting to check status of load operation when no load is in progress!")
+		return -1.0
+	var LoaderProgress: Array
+	var LoaderStatus = ResourceLoader.load_threaded_get_status(CurrentLoadingScenePath, LoaderProgress)
+	if (LoaderStatus == ResourceLoader.ThreadLoadStatus.THREAD_LOAD_FAILED or len(LoaderProgress) < 1):
+		push_error("Load operation failed!")
+		return -1.0
+	if (LoaderStatus == ResourceLoader.ThreadLoadStatus.THREAD_LOAD_FAILED):
+		push_error("Load operation failed due to invalid resource!")
+		return -1.0
+	if (LoaderStatus == ResourceLoader.ThreadLoadStatus.THREAD_LOAD_LOADED):
+		SceneReadyToInstance = true
+	return LoaderProgress[0]
+
+# One of the bread-and-butter function of the manager. Finalizes the load operation and sets the loaded scene
+# to be the new tree root.
+func FinalizeSceneLoad():
+	if(CurrentLoadingScenePath == ""):
+		push_error("Attempting to finish load operation when no load is in progress!")
+		return
+	if(!SceneReadyToInstance):
+		push_error("Attempting to finalize load operation before load operation is complete!")
+		return
+	var LoadedScene: PackedScene = ResourceLoader.load_threaded_get(CurrentLoadingScenePath)
+	CurrentLoadingScenePath = ""
+	SceneReadyToInstance = false
+	get_tree().change_scene_to_packed.call_deferred(LoadedScene)
 
 # Adds a single scene to the registry. Mostly used as a helper
 # function by the function that does this en mass.
-func AddSceneToRegistry(SceneName: String, SceneFile: PackedScene):
+func AddSceneToRegistry(SceneName: String, ScenePath: String):
 	if(SceneRegistry.keys().find(SceneName) != -1):
-		push_error("Attempting to add scene to registry with an in-use key!")
+		push_error("Attempting to add scene to registry with in-use name: " + SceneName)
 		return
-	SceneRegistry[SceneName] = SceneFile
+	if(!ResourceLoader.exists(ScenePath, "PackedScene")):
+		push_error("Attempting to add scene to registry with invalid scene path: " + ScenePath)
+		return
+	# Due to a quirk of the exists function, we still have to make sure we're looking at a .tscn
+	if(ScenePath.get_extension() != "tscn"):
+		push_error("Attempting to add scene to registry with invalid scene path: " + ScenePath)
+		return
+	SceneRegistry[SceneName] = ScenePath
 	print(SceneRegistry)
 
 # Adds a dictionary of scenes to the registry. This will mostly
 # be used by the initializer.
-func AddScenesToRegistry(Scenes: Dictionary[String, PackedScene]):
+func AddScenesToRegistry(Scenes: Dictionary[String, String]):
 	for SceneName in Scenes.keys():
 		AddSceneToRegistry(SceneName, Scenes[SceneName])
 
 # Doubt that this will be necessary, but included for the sake of
 # completeness. Redirects a scene name in the registry to point
 # at a new PackedScene.
-func ModifySceneInRegistry(SceneName: String, SceneFile: PackedScene):
+func ModifySceneInRegistry(SceneName: String, ScenePath: String):
 	if(SceneRegistry.keys().find(SceneName) == -1):
-		push_error("Attempting to modify unregistered scene!")
+		push_error("Attempting to modify unregistered scene with name: " + SceneName)
 		return
-	SceneRegistry[SceneName] = SceneFile
+	if(!ResourceLoader.exists(ScenePath, "PackedScene")):
+		push_error("Attempting to modify scene in registry to point to invalid scene path: " + ScenePath)
+		return
+	# Due to a quirk of the exists function, we still have to make sure we're looking at a .tscn
+	if(ScenePath.get_extension() != "tscn"):
+		push_error("Attempting to modify scene in registry with invalid scene path: " + ScenePath)
+		return
+	SceneRegistry[SceneName] = ScenePath
 
 # Same as the above for a group of scenes.
-func ModifyScenesInRegistry(Scenes: Dictionary[String, PackedScene]):
+func ModifyScenesInRegistry(Scenes: Dictionary[String, String]):
 	for SceneName in Scenes.keys():
 		ModifySceneInRegistry(SceneName, Scenes[SceneName])
 
@@ -58,7 +107,7 @@ func ModifyScenesInRegistry(Scenes: Dictionary[String, PackedScene]):
 # completeness. Removes a scene from the registry
 func RemoveSceneFromRegistry(SceneName: String):
 	if(SceneRegistry.keys().find(SceneName) == -1):
-		push_error("Attempting to remove unregistered scene!")
+		push_error("Attempting to remove unregistered scene with name: " + SceneName)
 		return
 	SceneRegistry.erase(SceneName)
 
